7a35e9a6a254a8da32f16a74e31aeb6c67b32493
m11o <velonica2227@icloud.com>
2019-08-26 19:54:00 +0900

    refactor: console.logの削除

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
-const path = require('path');
-
+const path        = require('path');
 const output_path = path.resolve(__dirname, 'dist');
-console.log(output_path);
 
 module.exports = {
   entry: './src/index.js',
